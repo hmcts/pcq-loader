7483bf6aec84ff8d789db28f638b5a0ccdc45013
Priyanka Verma <priyanka.verma@hmcts.net>
2026-04-24 15:47:19 +0100

    PCQ-2244 Adding MDC key for all the logs to use for alert

--- src/main/java/uk/gov/hmcts/reform/pcqloader/PcqLoaderApplication.java
+++ src/main/java/uk/gov/hmcts/reform/pcqloader/PcqLoaderApplication.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pcqloader;
 import com.microsoft.applicationinsights.TelemetryClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
+
+import static uk.gov.hmcts.reform.pcqloader.utils.PcqLoaderConstants.PCQ_LOADER_ERROR_MARKER;
 
 
 @SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform")
@@ -35,13 +38,10 @@ public class PcqLoaderApplication implements ApplicationRunner {
             pcqLoaderComponent.execute();
             log.info("Completed the Pcq Loader job successfully.");
         } catch (Exception e) {
-            //This specific message error has been added in Azure log to look for these traces in alert
-            // query and create alert if disposer-idam-user throw any exception because of any reason.
-            log.error("Error executing Pcq Loader : " +  e);
-            //To have stack trace of this exception as we are catching the exception
-            // stack trace will not be logged by azure
+            MDC.put("errorType", PCQ_LOADER_ERROR_MARKER);
             log.error("Error executing Pcq Loader", e);
         } finally {
+            MDC.clear();
             client.flush();
             waitTelemetryGracefulPeriod();
         }
---
+++ src/main/java/uk/gov/hmcts/reform/pcqloader/utils/PcqLoaderConstants.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.pcqloader.utils;
+
+public final class PcqLoaderConstants {
+    public static final String PCQ_LOADER_ERROR_MARKER = "PcqLoaderError: ";
+
+    private PcqLoaderConstants() {
+         // Private constructor to prevent instantiation
+    }
+}
